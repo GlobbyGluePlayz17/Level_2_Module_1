e32090c5c29976ffaf7e7609b855ee90e6bd897e
GlobbyGluePlayz17 <league@wts14.attlocal.net>
2018-06-09 15:55:28 -0700

    IntroToArrayList

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,40 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> fruit = new ArrayList<String>();
+		// 2. Add five no 6 Strings to your list
+		fruit.add("stirng");
+		fruit.add("stinrg");
+		fruit.add("stirng");
+		fruit.add("histringin");
+		fruit.add("blank");
+		fruit.add("ihadtomakethiscuzof#7needsane");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < fruit.size(); i++) {
+			System.out.println(fruit.get(i));
+		}
+		// 4. Print all the Strings using a for-each loop
+		for (String s : fruit) {
+			System.out.println(s);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < fruit.size(); i += 2) {
+			System.out.println(fruit.get(i));
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = fruit.size() - 1; i >= 0; i--) {
+			System.out.println(fruit.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (String s : fruit) {
+			if (s.contains("e")) {
+				System.out.println(s);
+			}
+		}
 	}
 }
